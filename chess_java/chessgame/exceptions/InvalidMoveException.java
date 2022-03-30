a291112235d6683cca5c12db4ff85c6e226feb7f
Domi <domonkos.maller@nokia.com>
2022-03-30 11:48:34 +0200

    asd

--- chess_java/chessgame/exceptions/InvalidMoveException.java
+++
@@ -1,7 +0,0 @@
-package hu.ppke.itk.java2020.maldo.hf03.chessgame.exceptions;
-
-public class InvalidMoveException extends Exception {
-    public InvalidMoveException(String type){
-        super(type);
-    }
-}
--- chess_java/chessgame/misc/Color.java
+++
@@ -1,6 +0,0 @@
-package hu.ppke.itk.java2020.maldo.hf03.chessgame.misc;
-
-public enum Color {
-    WHITE,
-    BlACK
-}
